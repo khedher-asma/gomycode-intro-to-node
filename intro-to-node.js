07fbfccc615d3a5d6fbb71cee13d4e8c81825581
khedher-asma <khedherasma90@gmail.com>
2019-07-07 15:55:45 +0200

    second commit

--- intro-to-node.js
+++ intro-to-node.js
@@ -4,8 +4,8 @@ console.log("HELLO WORLD")
 
 // exercice 2
 let sum = 0
-for (let i=2; i<process.argv.length; i++){
-    sum+=+process.argv[i]
+for (let i = 2; i < process.argv.length; i++) {
+    sum += +process.argv[i]
 }
 console.log(sum)
 
@@ -13,24 +13,24 @@ console.log(sum)
 let fs = require('fs')
 let str = fs.readFileSync(process.argv[2])
 let array = str.toString().split('\n')
-console.log(array.length-1) 
+console.log(array.length - 1)
 
 //exercice 4
 let fs = require('fs')
-fs.readFile(process.argv[2],'utf-8',(err,data) => {
-    if(err) throw err;
-    let lines = data.split('\n').length-1
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+    if (err) throw err;
+    let lines = data.split('\n').length - 1
     console.log(lines)
 })
 
 // exercice 5 methode 1
 let fs = require('fs')
-const filtreTexte = (arr, requete) => { 
-    return arr.filter(el =>  el.toLowerCase().indexOf('.'+requete.toLowerCase()) !== -1);
-  }
-fs.readdir(process.argv[2],(err,files) => {
-    if(err) console.log(err)
-    let array = filtreTexte(files,process.argv[3])
+const filtreTexte = (arr, requete) => {
+    return arr.filter(el => el.toLowerCase().indexOf('.' + requete.toLowerCase()) !== -1);
+}
+fs.readdir(process.argv[2], (err, files) => {
+    if (err) console.log(err)
+    let array = filtreTexte(files, process.argv[3])
     array.forEach(element => {
         console.log(element)
     });
@@ -43,7 +43,7 @@ var path = require('path')
 fs.readdir(process.argv[2], (err, files) => {
     if (err) return console.error(err)
     files.forEach(file => {
-        if (path.extname(file).toLowerCase() ===  '.' + process.argv[3]) {
+        if (path.extname(file).toLowerCase() === '.' + process.argv[3]) {
             console.log(file)
         }
     })
@@ -51,9 +51,70 @@ fs.readdir(process.argv[2], (err, files) => {
 
 // exercice 6
 const filtredModule = require('./filtred-module')
-filtredModule(process.argv[2], process.argv[3],(err,files) => {
-    if(err) console.log(err)
+filtredModule(process.argv[2], process.argv[3], (err, files) => {
+    if (err) console.log(err)
     files.forEach(element => {
         console.log(element)
     });
+})
+
+// exercice 7
+let http = require('http')
+let urlPath = process.argv[2]
+http.get(urlPath, (res) => {
+    const { statusCode } = res;
+
+    if (statusCode !== 200) {
+        console.error('Request Failed.\n' +
+            `Status Code: ${statusCode}`);
+        res.resume();
+        return;
+    }
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => { console.log(chunk); });
+}).on('error', (e) => {
+    console.error(`Got error: ${e.message}`);
+});
+
+//exercice 8 method 1 
+let http = require('http')
+let urlPath = process.argv[2]
+
+http.get(urlPath, (res) => {
+    const { statusCode } = res;
+
+    if (statusCode !== 200) {
+        console.error('Request Failed.\n' +
+            `Status Code: ${statusCode}`);
+        res.resume();
+        return;
+    }
+    res.setEncoding('utf8');
+    let rawData = '';
+    let dataLength = 0;
+    res.on('data', (chunk) => {
+        rawData += chunk;
+        dataLength += chunk.length
+    });
+    res.on('end', () => {
+        console.log(dataLength);
+        console.log(rawData);
+    });
+}).on('error', (e) => {
+    console.error(`Got error: ${e.message}`);
+});
+
+//exercice 8 method 2
+var http = require('http')
+var bl = require('bl')
+
+http.get(process.argv[2], function (response) {
+    response.pipe(bl(function (err, data) {
+        if (err) {
+            return console.error(err)
+        }
+        data = data.toString()
+        console.log(data.length)
+        console.log(data)
+    }))
 })
